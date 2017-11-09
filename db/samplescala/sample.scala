00c5ffca57fffb49f53ba745217764f4af8389b0
bgbigdata <tak.fuji123abc@gmail.com>
2017-11-09 08:16:24 +0000

    dbサンプルの更新。日本語上手く出ず ibaraki

--- db/samplescala/sample.scala
+++ db/samplescala/sample.scala
@@ -1,9 +1,11 @@
 package Hello
 
 import java.sql.{DriverManager, Connection, Statement, ResultSet, SQLException}
+import java.io.PrintWriter
 
 object SqlSelect {
   def main(args : Array[String]) : Unit = {
+	val out1 = new PrintWriter("out.txt")
     try {
         Class.forName("com.mysql.jdbc.Driver").newInstance();
         var con = 
@@ -17,7 +19,12 @@ object SqlSelect {
               print(rs.getString(2) + " ")
               print(rs.getString(3) + " ")
               println(rs.getString(4))
-          }
+			out1.write(rs.getString(1) + " ")
+			out1.write(rs.getString(2) + " ")
+			out1.write(rs.getString(3) + " ")
+			out1.write(rs.getString(4) + "\r\n")
+          }          
+//           var rs = stmt.executeUpdate("INSERT INTO rawtext VALUES('0x11112','twitter','1','日本語ニュース')")
           stmt.close()
       } catch {
          case e:SQLException => println("Database error "+e)
@@ -28,6 +35,7 @@ object SqlSelect {
          }
       } finally {
          con.close()
+         out1.close()
       }
    } catch {
       case e:SQLException => println("Database error "+e)
