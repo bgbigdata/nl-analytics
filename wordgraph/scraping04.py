8da46791da40e9861a207c97dbf352fb2af193ac
bgbigdata <32726390+bgbigdata@users.noreply.github.com>
2017-12-17 23:15:16 +0900

    Update scraping04.py
    ジャンル追加

--- wordgraph/scraping04.py
+++ wordgraph/scraping04.py
@@ -10,19 +10,71 @@ from time import sleep
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+
 domain = "https://senken.co.jp"
-startpath = "/categories/general"
+startpath1 = "/categories/general"
+startpath2 = "/categories/corporation"
+startpath3 = "/categories/hr"
+startpath4 = "/categories/trend"
+startpath5 = "/categories/product"
+startpath6 = "/categories/startup"
+startpath7 = "/categories/supply"
+startpath8 = "/categories/education"
+startpath9 = "/categories/sales"
+startpath10 = "/categories/carrer"
+startpath11 = "/categories/report"
+startpath12 = "/categories/senkenjob"
+startpath13 = "/categories/column"
+startpath14 = "/categories/technical-terms"
 
-basesoup = BeautifulSoup(requests.get(domain + startpath).text, "lxml")
-
-
+basesoup1 = BeautifulSoup(requests.get(domain + startpath1).text, "lxml")
+basesoup2 = BeautifulSoup(requests.get(domain + startpath2).text, "lxml")
+basesoup3 = BeautifulSoup(requests.get(domain + startpath3).text, "lxml")
+basesoup4 = BeautifulSoup(requests.get(domain + startpath4).text, "lxml")
+basesoup5 = BeautifulSoup(requests.get(domain + startpath5).text, "lxml")
+basesoup6 = BeautifulSoup(requests.get(domain + startpath6).text, "lxml")
+basesoup7 = BeautifulSoup(requests.get(domain + startpath7).text, "lxml")
+basesoup8 = BeautifulSoup(requests.get(domain + startpath8).text, "lxml")
+basesoup9 = BeautifulSoup(requests.get(domain + startpath9).text, "lxml")
+basesoup10 = BeautifulSoup(requests.get(domain + startpath10).text, "lxml")
+basesoup11 = BeautifulSoup(requests.get(domain + startpath11).text, "lxml")
+basesoup12 = BeautifulSoup(requests.get(domain + startpath12).text, "lxml")
+basesoup13 = BeautifulSoup(requests.get(domain + startpath13).text, "lxml")
+basesoup14 = BeautifulSoup(requests.get(domain + startpath14).text, "lxml")
 # In[9]:
 
 
 links = []
-for a in basesoup.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
+for a in basesoup1.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
+    links.append(domain + a.get("href"))
+for a in basesoup2.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
+    links.append(domain + a.get("href"))
+for a in basesoup3.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
+    links.append(domain + a.get("href"))
+for a in basesoup4.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
+    links.append(domain + a.get("href"))
+for a in basesoup5.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
+    links.append(domain + a.get("href"))
+for a in basesoup6.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
+    links.append(domain + a.get("href"))
+for a in basesoup7.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
+    links.append(domain + a.get("href"))
+for a in basesoup8.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
+    links.append(domain + a.get("href"))
+for a in basesoup9.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
+    links.append(domain + a.get("href"))
+for a in basesoup10.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
+    links.append(domain + a.get("href"))
+for a in basesoup11.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
+    links.append(domain + a.get("href"))
+for a in basesoup12.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
+    links.append(domain + a.get("href"))
+for a in basesoup13.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
+    links.append(domain + a.get("href"))
+for a in basesoup14.find_all("a", href=re.compile("/posts/[a-zA-Z0-9\._¥+-]")):
     links.append(domain + a.get("href"))
 links = list(set(links))
+
 
 #print(links)
 
